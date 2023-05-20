3a80606aabf6b0bf950f3925c720a75ca239a4ef
hawon0112 <131540587+hawon0112@users.noreply.github.com>
2023-05-20 23:22:00 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,33 +1,37 @@
+function LinkssetColor(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+    console.log(alist[i]);
+    alist[i].style.color = color;
+    i=i + 1;
+    }
+}
+var Body = {
+    setColor:function (color){
+        document.querySelector('body').style.color=color;
+    },
+    
+    setBackgroundColor:function (color){
+        document.querySelector('body').style.backgroundColor=color;
+    }
+}
 function nightDayHandler(self){
     var target = document.querySelector('body');
                 var target2 = document.querySelector('#title');
                 if(self.value === 'night'){
-                target.style.backgroundColor='black';
-                target.style.color='white';
+                Body.SetBackgroundColor('black');
+                Body.SetColor('white');
                 target2.style.color='white';
                 self.value = 'day';
-                var alist = document.querySelectorAll('a');
-                var i = 0;
-
-                while(i < alist.length){
-                    console.log(alist[i]);
-                    alist[i].style.color = 'powderblue';
-                    i=i + 1;
-                }
+                setColor('powderblue')
             } else {
-                target.style.backgroundColor='white';
-                target.style.color='black';
+                Body.SetBackgroundColor('white');
+                Body.SetColor('black');
                 target2.style.color='black';
                 self.value = 'night';
 
                 
-                var alist = document.querySelectorAll('a');
-                var i = 0;
-                while(i < alist.length){
-                    console.log(alist[i]);
-                    alist[i].style.color = 'gray';
-                    i=i + 1;
-                }
-                
+                setColor('blue')
             }
 }
